9eab594f20dc5b0b52f1bc88639f09655b011a9f
Augustine Oloche Noah <austinnoah92@gmail.com>
2025-01-14 12:18:30 +0100

    Updated

--- ML_Zoom_project.py
+++ ML_Zoom_project.py
@@ -1,6 +1,6 @@
 # %% [markdown]
 # ## 1. Problem Description
-# We are tasked with predicting the next day's closing price of Apple stock (AAPL) using historical stock data. The goal is to build a regression model to forecast future prices, leveraging features such as price change, moving averages, and volume changes. We will use both a Linear Regression model and a Random Forest Regressor to evaluate which model provides better predictions.
+# We are tasked with predicting the next day's closing price of Tesla Stock (TSLA) using historical stock data. The goal is to build a regression model to forecast future prices, leveraging features such as price change, moving averages, and volume changes. We will use both a Linear Regression model and a Random Forest Regressor to evaluate which model provides better predictions.
 # 
 
 # %% [markdown]
